996f1d380e2011eab2307ad771b1e200dfb9cf71
drnxv <87783549+drnxv@users.noreply.github.com>
2024-05-28 10:46:13 -0400

    front end changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,21 +7,31 @@ function App() {
       <header className="App-header">
         <h1>MealGPT</h1>
         <p className="instructions">
-          Enter your ingredients (separated by spaces or commas), a cuisine, and any dietary restrictions (e.g., vegetarian)
+          Enter your ingredients (separated by spaces or commas), a cuisine, and any dietary
+          restrictions (e.g., vegetarian)
         </p>
         <form className="meal-form">
-          <input type="text" placeholder="ingredients" className="input-field" required/>
+          <input type="text" placeholder="ingredients" className="input-field" required />
           <input type="text" placeholder="cuisine (optional)" className="input-field" />
-          <input type="text" placeholder="dietary restrictions (optional)" className="input-field" />
+          <input
+            type="text"
+            placeholder="dietary restrictions (optional)"
+            className="input-field"
+          />
           <button type="submit" className="generate-button">
             Generate
           </button>
         </form>
         <p className="download-instruction">
           Download your recipe here!{' '}
-        <img className="download-icon" src={downloadIcon} alt="Download" />
+          <img className="download-icon" src={downloadIcon} alt="Download" />
         </p>
       </header>
+      <div id="footer">
+        <footer>
+          <p id="copyright">© 2023 Pranav Peddamalla</p>
+        </footer>
+      </div>
     </div>
   );
 }
